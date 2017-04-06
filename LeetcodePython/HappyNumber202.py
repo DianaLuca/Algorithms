fc014f041663a72b14cb13fc8f0287110372790e
Diana Luca <lucadiana88@gmail.com>
2017-04-06 14:57:08 -0700

    Added Happy Number

---
+++ LeetcodePython/HappyNumber202.py
@@ -0,0 +1,27 @@
+# Write an algorithm to determine if a number is "happy".
+# A happy number is a number defined by the following process:
+# Starting with any positive integer, replace the number by the sum of the squares of its digits,
+# and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+#  Those numbers for which this process ends in 1 are happy numbers.
+
+
+
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        tmpl = list(str(n))
+        preSet = set()
+
+        while True:
+            res = 0
+            for el in tmpl:
+                res += int(el) * int(el)
+            if res == 1:
+                return True
+            if res in preSet:
+                return False
+            preSet.add(res)
+            tmpl = str(res)
