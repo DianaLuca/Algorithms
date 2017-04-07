ab40c3d7b7e38026178044aabafa2e87916f8caf
Diana Luca <lucadiana88@gmail.com>
2017-04-06 18:41:54 -0700

    Added Climbing Stairs algo

---
+++ LeetcodePython/ClimbingStairs70.py
@@ -0,0 +1,19 @@
+# You are climbing a stair case. It takes n steps to reach to the top.
+# Each time you can either climb 1 or 2 steps.
+# In how many distinct ways can you climb to the top?
+#
+# Note: Given n will be a positive integer.
+#
+# Fibonacci series
+
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        a, b = 1, 1
+
+        for i in range(n):
+            a, b = b, a + b
+        return a
