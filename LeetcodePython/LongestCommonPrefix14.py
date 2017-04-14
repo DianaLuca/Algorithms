416adb76c286686f522454149034d23d959fda12
Diana Luca <lucadiana88@gmail.com>
2017-04-14 12:01:41 -0700

    Added Longest Common Prefix and Merge Sorted Array

---
+++ LeetcodePython/LongestCommonPrefix14.py
@@ -0,0 +1,29 @@
+# Write a function to find the longest common prefix string amongst an array of strings.
+
+import sys
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+
+        res = ""
+        if not strs:
+            return res
+
+        minel = sys.maxsize
+        for el in strs:
+            if len(el) < minel:
+                minel = len(el)
+                small = el
+
+        for i in range(len(small)):
+            for j in range(len(strs)):
+                if strs[j][i] != small[i]:
+                    return res
+                if j == len(strs) - 1 and strs[j][i] == small[i]:
+                    res += small[i]
+
+        return res
